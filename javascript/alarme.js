967be41fbbef0ee5259b9e9dd507d8f32c15f823
firzek <84140749+firzek@users.noreply.github.com>
2021-06-05 18:22:22 -0300

    upload para o sprint3
    mudanças nos botões no alarme.html para encaixar no jscript e upload no alarme.js

--- javascript/alarme.js
+++ javascript/alarme.js
@@ -6,11 +6,15 @@ var botaoQ = 1;
 var botaoQui = 1;
 var botaoSe = 1;
 var botaoSab = 1;
+var botaoSoneca = 1;
+var botaoFeriado = 0;
+
+
 onload = () => {
     document.querySelector('#botaosalvar').onclick = function () {
         alert("alarme salvo com sucesso!");
-        let alarmeHoras = document.getElementById("horas").value;
-        let alarmeMinutos = document.getElementById("minutos").value;
+        let alarmeHoras = 0;
+        let alarmeMinutos = 0;
         let alarmeDias = new Array();
         alarmeDias[0] = botaoD;
         alarmeDias[1] = botaoS;
@@ -19,34 +23,42 @@ onload = () => {
         alarmeDias[4] = botaoQui;
         alarmeDias[5] = botaoSe;
         alarmeDias[6] = botaoSab;
+        let soneca = document.getElementById("soneca").value;
         let nomeDoAlarme = document.getElementById("nomeAlarme").value;
         console.log(nomeDoAlarme);
         let alarmeNotas = document.getElementById("notasDoAlarmeBox").value;
-        let grupoAlarmes = new Array();
-        let anotacoesAlarme=document.getElementById("anotaçoesDoAlarmeBox").value;
+        let feriado= botaoFeriado;
+        let anotacoesAlarme = "nada";
         if (contador == 15) {
             alert('Limite de alarme alcançado');
         }
 
         else {
-            grupoAlarmes[contador] = criarAlarme(alarmeHoras, alarmeMinutos, alarmeDias, nomeDoAlarme, alarmeNotas,anotacoesAlarme);
+            var novoAlarme = criarAlarme(alarmeHoras, alarmeMinutos, alarmeDias, nomeDoAlarme, alarmeNotas, anotacoesAlarme, soneca,feriado);
             contador++;
         }
 
 
-        console.log(grupoAlarmes[contador - 1]);
+        console.log(novoAlarme);
         console.log(contador);
         console.log(botaoD);
+        let convert = JSON.stringify(novoAlarme);
+        console.log(convert);
+
+
+
     }
 
-    function criarAlarme(chave1, chave2, chave3, chave4, chave5,chave6) {
+    function criarAlarme(chave1, chave2, chave3, chave4, chave5, chave6, chave7,chave8) {
         let newAlarme = {
             "horas": chave1,
             "minutos": chave2,
             "dias": chave3,
             "nome": chave4,
             "notas": chave5,
-            "anotacoes":chave6
+            "anotacoes": chave6,
+            "soneca": chave7,
+            "feriado?":chave8,
         }
         return newAlarme;
     }
@@ -66,105 +78,131 @@ onload = () => {
 
 
     }
-    document.getElementById("botaoDomingo").onclick = botaoD = ligarBotao(botaoD);
-    document.getElementById("botaoDomingo").onclick = function () {
+    document.getElementById("botaoD").onclick = botaoD = ligarBotao(botaoD);
+    document.getElementById("botaoD").onclick = function () {
         if (botaoD == 0) {
-            document.getElementById("botaoDomingo").style.background = "#2B5C7C";
+            document.getElementById("botaoD").style.background = "#2B5C7C";
             botaoD = 1;
         }
         else {
-            document.getElementById("botaoDomingo").style.background = "#C30000";
+            document.getElementById("botaoD").style.background = "#C30000";
             botaoD = 0;
         }
 
     }
 
 
-    document.getElementById("botaoSegunda").onclick = function () {
+    document.getElementById("botaoS").onclick = function () {
         if (botaoS == 0) {
-            document.getElementById("botaoSegunda").style.background = "#2B5C7C";
+            document.getElementById("botaoS").style.background = "#2B5C7C";
             botaoS = 1;
         }
         else {
-            document.getElementById("botaoSegunda").style.background = "#C30000";
+            document.getElementById("botaoS").style.background = "#C30000";
             botaoS = 0;
         }
 
     }
 
-    document.getElementById("botaoTerca").onclick = function () {
+    document.getElementById("botaoT").onclick = function () {
         if (botaoT == 0) {
-            document.getElementById("botaoTerca").style.background = "#2B5C7C";
+            document.getElementById("botaoT").style.background = "#2B5C7C";
             botaoT = 1;
         }
         else {
-            document.getElementById("botaoTerca").style.background = "#C30000";
+            document.getElementById("botaoT").style.background = "#C30000";
             botaoT = 0;
         }
 
     }
 
-    document.getElementById("botaoTerca").onclick = function () {
+    document.getElementById("botaoT").onclick = function () {
         if (botaoT == 0) {
-            document.getElementById("botaoTerca").style.background = "#2B5C7C";
+            document.getElementById("botaoT").style.background = "#2B5C7C";
             botaoT = 1;
         }
         else {
-            document.getElementById("botaoTerca").style.background = "#C30000";
+            document.getElementById("botaoT").style.background = "#C30000";
             botaoT = 0;
         }
 
     }
 
-    document.getElementById("botaoQuarta").onclick = function () {
+    document.getElementById("botaoQ").onclick = function () {
         if (botaoQ == 0) {
-            document.getElementById("botaoQuarta").style.background = "#2B5C7C";
+            document.getElementById("botaoQ").style.background = "#2B5C7C";
             botaoQ = 1;
         }
         else {
-            document.getElementById("botaoQuarta").style.background = "#C30000";
+            document.getElementById("botaoQ").style.background = "#C30000";
             botaoQ = 0;
         }
 
     }
-    document.getElementById("botaoQuinta").onclick = function () {
+    document.getElementById("botaoQui").onclick = function () {
         if (botaoQui == 0) {
-            document.getElementById("botaoQuinta").style.background = "#2B5C7C";
+            document.getElementById("botaoQui").style.background = "#2B5C7C";
             botaoQui = 1;
         }
         else {
-            document.getElementById("botaoQuinta").style.background = "#C30000";
+            document.getElementById("botaoQui").style.background = "#C30000";
             botaoQui = 0;
         }
 
     }
-    document.getElementById("botaoSexta").onclick = function () {
+    document.getElementById("botaoSe").onclick = function () {
         if (botaoSe == 0) {
-            document.getElementById("botaoSexta").style.background = "#2B5C7C";
+            document.getElementById("botaoSe").style.background = "#2B5C7C";
             botaoSe = 1;
         }
         else {
-            document.getElementById("botaoSexta").style.background = "#C30000";
+            document.getElementById("botaoSe").style.background = "#C30000";
             botaoSe = 0;
         }
 
     }
-    document.getElementById("botaoSabado").onclick = function () {
+    document.getElementById("botaoSab").onclick = function () {
         if (botaoSab == 0) {
-            document.getElementById("botaoSabado").style.background = "#2B5C7C";
+            document.getElementById("botaoSab").style.background = "#2B5C7C";
             botaoSab = 1;
+
         }
         else {
-            document.getElementById("botaoSabado").style.background = "#C30000";
+            document.getElementById("botaoSab").style.background = "#C30000";
             botaoSab = 0;
+
         }
 
     }
+    document.getElementById("botaoSonecaToggle").onclick = function () {
+        if (botaoSoneca == 0) {
+            document.getElementById("sonecaPlace").style.background = "#2B5C7C";
+            botaoSoneca = 1;
+
+        }
+        else {
+            document.getElementById("sonecaPlace").style.background = "#C30000";
+            botaoSoneca = 0;
+
+        }
+    }
+    document.getElementById("botaoFeriadoToggle").onclick = function () {
+        if (botaoFeriado == 0) {
+
+            botaoFeriado = 1;
+            console.log(botaoFeriado)
+        }
+        else {
+
+            botaoFeriado = 0;
+
+        }
 
 
 
 
+
+
+    }
 
 }
-
-
