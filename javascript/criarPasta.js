4dc96a18f8ecb5cdff3e0974dfcf673f2a2f011e
firzek <84140749+firzek@users.noreply.github.com>
2021-06-23 09:27:15 -0300

    Update criarPasta.js
    -troca de variavel em alarme.js

--- javascript/criarPasta.js
+++ javascript/criarPasta.js
@@ -5,23 +5,13 @@ onload =()=>{
 
     
     
-    let newAlarme = {
-        "horas": 0,
-        "minutos": 0,
-        "dias": 0,
-        "nome": 0,
-        "notas": 0,
-        "anotacoes": 0,
-        "soneca": 0,
-        "feriado?": 0,
-        "pasta":0,
-        "som":0
-    }
+    let newAlarme 
+    
    
 
 document.getElementById('pastaSalvar').onclick = function (){
     alert("Pasta Salva com Sucesso");
-    newAlarme.pasta=document.getElementById('nomePasta').value;
+    newAlarme=document.getElementById('nomePasta').value;
      pastaNumero++;
     var pasta=new Array;
     var pastaNome="A"+pastaNumero;
