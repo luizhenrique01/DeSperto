2f78e8f4bd4bba2e09630561f800539e2088183d
firzek <84140749+firzek@users.noreply.github.com>
2021-06-24 10:36:48 -0300

    update javascript
    -correção onload predefinido

--- javascript/pastas.js
+++ javascript/pastas.js
@@ -1,3 +1,25 @@
+function click1(){
+    window.location.assign("telaAlarmes.html");
+    localStorage.setItem("pastaAtual","1")
+    
+}
+function click2(){
+    window.location.assign("telaAlarmes.html");
+    localStorage.setItem("pastaAtual","2")
+
+}
+function click3(){
+    window.location.assign("telaAlarmes.html");
+    localStorage.setItem("pastaAtual","3")
+}
+function click4(){
+    window.location.assign("telaAlarmes.html");
+    localStorage.setItem("pastaAtual","4")
+}
+function click5(){
+    window.location.assign("telaAlarmes.html");
+    localStorage.setItem("pastaAtual","5")
+}
 onload = () =>{
    
     let pastaNumero=localStorage.getItem("pastaNumero");
@@ -5,6 +27,7 @@ onload = () =>{
     if (pastaNumero==1){ 
         let h3 = document.createElement("h3");
         h3.setAttribute('id','1pasta');
+        h3.onclick = new Function('click1()');
         let alarmeData1=JSON.parse(localStorage.getItem('1'));
         let nome=alarmeData1;
         h3.innerHTML = nome;
@@ -17,6 +40,7 @@ onload = () =>{
       let h3 = document.createElement("h3");
         let alarmeData1=JSON.parse(localStorage.getItem('1'));
         h3.setAttribute('id','1pasta');
+        h3.onclick = new Function('click1()');
         let nome=alarmeData1;
         h3.innerHTML = nome;
             
@@ -27,6 +51,7 @@ onload = () =>{
         nome=alarmeData2;
         h3.innerHTML = nome;
         h3.setAttribute('id','2pasta');
+        h3.onclick = new Function('click2()');
             
             document.getElementById("pasta2").append(h3);
         
@@ -35,6 +60,7 @@ onload = () =>{
         let h3 = document.createElement("h3");
         let alarmeData1=JSON.parse(localStorage.getItem('1'));
         h3.setAttribute('id','1pasta');
+        h3.onclick = new Function('click1()');
         let nome=alarmeData1;
         h3.innerHTML = nome;
             
@@ -46,6 +72,7 @@ onload = () =>{
         nome=alarmeData2;
         h3.innerHTML = nome;
         h3.setAttribute('id','2pasta');
+        h3.onclick = new Function('click2()');
         document.getElementById("pasta2").append(h3);
             
            
@@ -54,6 +81,7 @@ onload = () =>{
             nome=alarmeData3;
             h3.innerHTML = nome;
             h3.setAttribute('id','3pasta');
+            h3.onclick = new Function('click3()');
                 
                 document.getElementById("pasta3").append(h3);
             
@@ -64,6 +92,7 @@ onload = () =>{
         let h3 = document.createElement("h3");
         let alarmeData1=JSON.parse(localStorage.getItem('1'));
         h3.setAttribute('id','1pasta');
+        h3.onclick = new Function('click1()');
         let nome=alarmeData1;
         h3.innerHTML = nome;
             
@@ -74,6 +103,7 @@ onload = () =>{
         nome=alarmeData2;
         h3.innerHTML = nome;
         h3.setAttribute('id','2pasta');
+        h3.onclick = new Function('click2()');
             
             document.getElementById("pasta2").append(h3);
             h3 = document.createElement("h3");
@@ -81,6 +111,7 @@ onload = () =>{
             nome=alarmeData3;
             h3.innerHTML = nome;
             h3.setAttribute('id','3pasta');
+            h3.onclick = new Function('click3()');
                 
                 document.getElementById("pasta3").append(h3);
                 h3 = document.createElement("h3");
@@ -88,6 +119,7 @@ onload = () =>{
                 nome=alarmeData4;
                 h3.innerHTML = nome;
                 h3.setAttribute('id','4pasta');
+                h3.onclick = new Function('click4()');
                     
                     document.getElementById("pasta4").append(h3);
                 
@@ -97,6 +129,7 @@ onload = () =>{
         let h3 = document.createElement("h3");
         let alarmeData1=JSON.parse(localStorage.getItem('1'));
         h3.setAttribute('id','1pasta');
+        h3.onclick = new Function('click1()');
         let nome=alarmeData1;
         h3.innerHTML = nome;
             
@@ -107,6 +140,7 @@ onload = () =>{
         nome=alarmeData2;
         h3.innerHTML = nome;
         h3.setAttribute('id','2pasta');
+        h3.onclick = new Function('click2()');
             
             document.getElementById("pasta2").append(h3);
             h3 = document.createElement("h3");
@@ -114,6 +148,7 @@ onload = () =>{
             nome=alarmeData3;
             h3.innerHTML = nome;
             h3.setAttribute('id','3pasta');
+            h3.onclick = new Function('click3()');
                 
                 document.getElementById("pasta3").append(h3);
                 h3 = document.createElement("h3");
@@ -121,6 +156,7 @@ onload = () =>{
                 nome=alarmeData4;
                 h3.innerHTML = nome;
                 h3.setAttribute('id','4pasta');
+                h3.onclick = new Function('click4()');
                     
                     document.getElementById("pasta4").append(h3);
                     h3 = document.createElement("h3");
@@ -128,6 +164,7 @@ onload = () =>{
                     nome=alarmeData5;
                     h3.innerHTML = nome;
                     h3.setAttribute('id','5pasta');
+                    h3.onclick = new Function('click5()');
                         
                         document.getElementById("pasta5").append(h3);
     
@@ -136,26 +173,5 @@ onload = () =>{
     
     
     
-document.getElementById('1pasta').onclick=function(){
-    window.location.assign("telaAlarmes.html");
-    localStorage.setItem("pastaAtual","1")
-    
-}
-document.getElementById('2pasta').onclick=function(){
-    window.location.assign("telaAlarmes.html");
-    localStorage.setItem("pastaAtual","2")
 
-}
-document.getElementById('3pasta').onclick=function(){
-    window.location.assign("telaAlarmes.html");
-    localStorage.setItem("pastaAtual","3")
-}
-document.getElementById('4pasta').onclick=function(){
-    window.location.assign("telaAlarmes.html");
-    localStorage.setItem("pastaAtual","4")
-}
-document.getElementById('5pasta').onclick=function(){
-    window.location.assign("telaAlarmes.html");
-    localStorage.setItem("pastaAtual","5")
-}
 }
