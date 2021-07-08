7973bf30435d0fdde60c1c0da22a6c44c8378e7e
firzek <84140749+firzek@users.noreply.github.com>
2021-07-07 21:56:19 -0300

    v0.0
    -funcionalidades aplicadas

--- javascript/criarPasta.js
+++ javascript/criarPasta.js
@@ -2,11 +2,11 @@ var pastaNumero=0;
 pastaNumero=localStorage.getItem("pastaNumero");
 
 onload =()=>{
-    document.getElementById("voltar").onclick = function () {
+    document.getElementById("pastaSalvar").onclick = function () {
         
         window.location.assign("telaInterna.html")
     let newAlarme 
-
+    }
 document.getElementById('pastaSalvar').onclick = function (){
     alert("Pasta Salva com Sucesso");
     newAlarme=document.getElementById('nomePasta').value;
@@ -18,8 +18,7 @@ document.getElementById('pastaSalvar').onclick = function (){
     localStorage.setItem(pastaNumero, JSON.stringify(newAlarme));
     localStorage.setItem("pastaNumero",pastaNumero);
     localStorage.setItem(pastaNome,JSON.stringify(pasta));
-     window.location.assign( "telaInterna.html");
+     window.location.assign("telaInterna.html");
      
     }
-}
 }
